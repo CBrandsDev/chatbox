435f7d228200761fc5eb68a8ef050442e56f4323
CBrandsDev <cbrandao2001@gmail.com>
2024-05-25 11:54:20 -0300

    let's do this

--- Start.bat
+++ Start.bat
@@ -4,6 +4,9 @@ set endPort=8005
 
 for /L %%i in (%startPort%,1,%endPort%) do (
     echo Starting Chat Server on port %%i...
-    start "Chat Server on port %%i" java ChatServer %%i
+    start /b java ChatServer %%i
 )
-pause
+echo All servers started. Press any key to close all.
+pause >nul
+```
+</rewritten_file><|eot_id|>
